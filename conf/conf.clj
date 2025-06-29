d6fb6795fac457f1802fc326239840dc718c3dfd
sogaiu <983021772@users.noreply.github.com>
2025-06-29 11:15:07 +0900

    Sync up conf.clj with current intention

--- conf/conf.clj
+++ conf/conf.clj
@@ -9,12 +9,12 @@
 ;; tree-sitter-clojure
 
 ;; if ts-clojure is a subdir of tree-sitter-clojure
-(def grammar-dir 
+'(def grammar-dir
   (->> (fs/absolutize "..")
        fs/normalize
        (format "%s")))
 ;; if tree-sitter-clojure is a subdir of ts-clojure
-;(def grammar-dir "tree-sitter-clojure")
+(def grammar-dir "tree-sitter-clojure")
 
 ;; tree-sitter
 
